fd80d27574d1fcec22914d04e048f2b2fcb8c688
tran nhat <tranminhnhat1005@gmail.com>
2021-07-30 08:44:35 +0700

    master - add static file and setting node-sass, scss

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // morgan to view console log
 app.use(morgan('combined'));
 
